18c4920e4aa3d5c62628d0f1d7e84db9324c35f7
Fedor Baart <fedor.baart@deltares.nl>
2013-08-14 14:54:01 +0200

    first version

--- skill/__init__.py
+++ skill/__init__.py
@@ -5,7 +5,12 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+    # Use mako as default renderer
+    config.add_renderer(name='.html', factory="pyramid.mako_templating.renderer_factory")
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('experiment', '/experiment/{case}')
+    config.add_route('image', '/image/{id}')
+    config.add_route('detail', '/detail/{case}/{quantity}/{period}')
     config.scan()
     return config.make_wsgi_app()
--- skill/views.py
+++ skill/views.py
@@ -1,6 +1,51 @@
 from pyramid.view import view_config
-
-
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+import pyramid.response
+import models
+import PIL
+import cStringIO
+import random
+import numpy as np
+@view_config(route_name='home', renderer='main.html')
 def my_view(request):
+
     return {'project': 'skill'}
+
+
+@view_config(route_name='experiment', renderer='experiment.html')
+def experiment(request):
+    df = models.getdata()
+    case = request.matchdict.get('case')
+    data = df[df.case == case]
+    return {
+        'project': 'skill',
+        'case': case,
+        'df' : data
+    }
+@view_config(route_name='image')
+def image(request):
+    df = models.getdata()
+    row = df.ix[int(request.matchdict.get('id'))]
+    path = row['path']
+    img = PIL.Image.open(path)
+    converted_file = cStringIO.StringIO()
+    if 'thumbnail' in request.params:
+        img.thumbnail((256,256))
+    img.save(converted_file, format='png')
+    converted_file.seek(0)
+    response = pyramid.response.Response(body_file=converted_file, request=request, content_type='image/png')
+    return response
+
+@view_config(route_name="detail", renderer='detail.html')
+def detail(request):
+    case = request.matchdict.get('case')
+    period = request.matchdict.get('period')
+    quantity = request.matchdict.get('quantity')
+    df = models.getdata()
+    df = df[np.logical_and(df.case==case, df.period==period, df.quantity==quantity)]
+    return {
+        'case': case,
+        'period': period,
+        'quantity': quantity,
+        'df': df
+        }
+
